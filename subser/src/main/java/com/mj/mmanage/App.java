4f7913a49ec64a478e1b6a74235e2865b1da639f
hailong830905 <hailong0905@sina.com>
2018-07-19 18:22:06 +0800

    lhl windows
    启动

--- subser/src/main/java/com/mj/mmanage/App.java
+++ subser/src/main/java/com/mj/mmanage/App.java
@@ -1,5 +1,7 @@
 package com.mj.mmanage;
 
+import java.io.IOException;
+
 import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +10,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.mj.webchat.util.AccessTokenUtil;
 
 
 /**
@@ -30,12 +34,18 @@ public class App  extends WebMvcConfigurerAdapter
 	
     public static void main( String[] args )
     {
-    	logger.info("系统启动......");
-    	System.out.println("系统启");
+    	logger.info("系统开始启动......");
     	
         SpringApplication.run(App.class, args);
         
+        AccessTokenUtil.getWebChatAccessToken();
+		
+		try {
+			AccessTokenUtil.getJSAPITicket();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+        
         logger.info("系统启动成功...");
-        System.out.println("系统启成功");
     }
 }
